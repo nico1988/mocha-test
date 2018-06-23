055a23e3f0e6ac36624fff3a91257fc8f47c7664
nico1988 <410827024@qq.com>
2018-06-23 15:34:25 +0800

    javascript test

---
+++ test/appTest.js
@@ -0,0 +1,15 @@
+// const assert = require('assert')  nodejs
+const assert = require('chai').assert;
+const app = require('../app');
+const sayHello = require('../app').sayHello;
+
+describe('App',function () {
+    it('sayHello should return hello',function(){
+        let result = sayHello();
+        assert.equal(result, 'hello');
+    })
+    it('sayHello should return type string', function(){
+        let result = sayHello();
+        assert.typeOf(result,'string');
+    })
+})
